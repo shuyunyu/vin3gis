f36d597582fbc94bde4aa9e142f8c4e4cbfcbb6e
shuyunyu <shuyun.yu@cocos.com>
2022-12-06 23:27:40 +0800

    feat update test code.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ window.onload = () => {
 
     const initCameraPosition = new THREE.Vector3(118.156, 24.118, 1650000);
     // const initCameraPosition = new THREE.Vector3(0, 0, 1650000);
-    const initCameraOrientation = new THREE.Vector3(0, -78, 0);
+    const initCameraOrientation = new THREE.Vector3(0, -90, 0);
     const homeViewPort = new Vin3GIS.ViewPort(Vin3GIS.Cartographic.fromDegrees(initCameraPosition.x, initCameraPosition.y, initCameraPosition.z), Vin3GIS.Orientation.fromDegreeEulerAngles(initCameraOrientation));
     const mapViewer = new Vin3GIS.MapViewer({
         target: document.body,
