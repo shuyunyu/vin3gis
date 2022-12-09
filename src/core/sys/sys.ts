446e561a7892c84dd57b86f0f7ba2273f687baba
yushuyun <shuyun.yu@cocos.com>
2022-12-09 12:34:42 +0800

    feat update sys

--- src/core/sys/sys.ts
+++ src/core/sys/sys.ts
@@ -1,53 +1,49 @@
 import { BrowserType, OSType } from "../../@types/core/sys/sys";
 
+const userAgentContains = (str: string) => {
+    return navigator.userAgent.toLowerCase().includes(str);
+}
+
+const mac = navigator.platform.startsWith('Mac');
+const linux = navigator.platform.startsWith('Linux');
+const win = navigator.platform.startsWith('Win');
+
+const webkit = userAgentContains('webkit');
+
+const chrome = userAgentContains('chrome');
+const safari = !chrome && userAgentContains('safari');
+// @property gecko: Boolean; `true` for gecko-based browsers like Firefox.
+const gecko = userAgentContains('gecko') && !webkit;
+
+const isMobile = typeof orientation !== 'undefined' || userAgentContains('mobile');
+
+let os = OSType.UNKNOW;
+
+if (mac) {
+    os = OSType.MAC
+} else if (linux) {
+    os = OSType.LINUX;
+} else if (win) {
+    os = OSType.WIN;
+}
+
+let browser = BrowserType.UNKNOW;
+
+if (chrome) {
+    browser = BrowserType.CHROME;
+} else if (safari) {
+    browser = BrowserType.SAFARI;
+} else if (gecko) {
+    browser = BrowserType.FIRFOX;
+} else {
+    browser = BrowserType.UNKNOW;
+}
+
 /**
  * 系统信息
  */
-class Sys {
-
-    public os: OSType;
-
-    public browser: BrowserType;
-
-    public isMobile: boolean;
-
-    public constructor () {
-        const mac = navigator.platform.startsWith('Mac');
-        const linux = navigator.platform.startsWith('Linux');
-        const win = navigator.platform.startsWith('Win');
-        if (mac) {
-            this.os = OSType.MAC
-        } else if (linux) {
-            this.os = OSType.LINUX;
-        } else if (win) {
-            this.os = OSType.WIN;
-        } else {
-            this.os = OSType.UNKNOW;
-        }
-
-        this.isMobile = typeof orientation !== 'undefined' || this.userAgentContains('mobile');
-
-        const webkit = this.userAgentContains('webkit');
-
-        const chrome = this.userAgentContains('chrome');
-        const safari = !chrome && this.userAgentContains('safari');
-        // @property gecko: Boolean; `true` for gecko-based browsers like Firefox.
-        const gecko = this.userAgentContains('gecko') && !webkit;
-        if (chrome) {
-            this.browser = BrowserType.CHROME;
-        } else if (safari) {
-            this.browser = BrowserType.SAFARI;
-        } else if (gecko) {
-            this.browser = BrowserType.FIRFOX;
-        } else {
-            this.browser = BrowserType.UNKNOW;
-        }
-    }
-
-    public userAgentContains (str: string) {
-        return navigator.userAgent.toLowerCase().includes(str);
-    }
-
-}
-
-export const sys = new Sys();
+export const sys = Object.freeze({
+    os: os,
+    browser: browser,
+    isMobile: isMobile
+});
